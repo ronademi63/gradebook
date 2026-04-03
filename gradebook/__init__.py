94d099756bcdd41cdfb305ecc1f5899dc894116d
Ron Ademi <ronademi63@gmail.com>
2026-04-03 15:43:35 +0200

    initialize gradebook package with __init__.py

---
+++ gradebook/__init__.py
@@ -0,0 +1,10 @@
+import logging
+import os
+
+os.makedirs('logs', exist_ok=True)
+
+logging.basicConfig(
+    filename='logs/app.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
